5ac5b835d8d7281f66f08cee13c9b1bf913d1295
Ph <ph@Hs-MacBook-Pro.lan>
2022-07-08 15:57:04 +0100

    package.json and listen.js

--- listen.js
+++ listen.js
@@ -2,7 +2,8 @@ const app = require('./app');
 const { PORT = 9090 } = process.env;
 
 app.listen(PORT, (err) => {
-  if (err) throw err;
+  if (err){
+   throw err;
+  }  
   console.log(`Listening on ${PORT}...`);
-  }
-);
+})
